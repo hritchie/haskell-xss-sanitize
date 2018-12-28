91a197dd1078aae42ac3858f3d90ff03124567c2
DHC <danchoi+git@danielchoi.com>
2018-12-28 18:09:18 -0500

    comment

--- src/Text/HTML/SanitizeXSS.hs
+++ src/Text/HTML/SanitizeXSS.hs
@@ -42,6 +42,7 @@ import           Text.HTML.TagSoup
 -- Use to report errors
 data XssFlag = XssFlag Text -- to develop
 
+-- TODO change to Seq or something more performant
 type XssWriter = Writer [XssFlag]
 
 type XssTagFilter = [Tag Text] -> XssWriter [Tag Text]
