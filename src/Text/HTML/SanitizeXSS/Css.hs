6d73fdaeac888327e2bbb1be99674a1b4748d0c7
DHC <danchoi+git@danielchoi.com>
2018-12-29 14:25:04 -0500

    opacity should be allowed property

--- src/Text/HTML/SanitizeXSS/Css.hs
+++ src/Text/HTML/SanitizeXSS/Css.hs
@@ -138,7 +138,7 @@ allowed_css_properties = H.fromList $ acceptable_css_properties <> mso_css_prope
       "border-left-color", "border-right-color", "border-top-color", "clear",
       "color", "cursor", "direction", "display", "elevation", "float", "font",
       "font-family", "font-size", "font-style", "font-variant", "font-weight",
-      "height", "letter-spacing", "line-height", "overflow", "pause",
+      "height", "letter-spacing", "line-height", "opacity", "overflow", "pause",
       "pause-after", "pause-before", "pitch", "pitch-range", "richness",
       "speak", "speak-header", "speak-numeral", "speak-punctuation",
       "speech-rate", "stress", "text-align", "text-decoration", "text-indent",
