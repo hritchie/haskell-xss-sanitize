2b4ebb0112d1d82d694c81f639a163c2e37afd4f
DHC <danchoi+git@danielchoi.com>
2018-12-29 10:58:18 -0500

    clean up and comment

--- src/Text/HTML/SanitizeXSS/Types.hs
+++ src/Text/HTML/SanitizeXSS/Types.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Text.HTML.SanitizeXSS.Types where
-import Control.Monad.RWS.Lazy
-import           Data.Text                 (Text)
+import           Control.Lens
+import           Control.Monad.RWS.Lazy
+import           Data.Text              (Text)
 import           Text.HTML.TagSoup
-import Control.Lens
 
 
 
 -- Use to report errors
 data XssFlag = XssFlag {
       _flagPosition :: (Int, Int)
-    , _flagOpenTag :: Tag Text
-    , _flagReason :: Text -- position, open tag
+    , _flagOpenTag  :: Tag Text
+    , _flagReason   :: Text -- position, open tag
     }
   deriving (Show, Eq)
 
@@ -19,21 +19,23 @@ makeLenses ''XssFlag
 
 
 -- TODO change to Seq or something more performant
--- TODO we want state monad so we know if we are IN an unsafe tag like script
+-- we want state monad so we know if we are IN an unsafe tag like script
 -- because it is better to suppress and report the content of script tag
 
 -- are we in an unsafe tag?
 data XssState = XssState {
-    _unsanitaryTagStack :: [Tag Text] -- stack of open unsanitary tags
+    -- not sure if we want to keep a tag stack here,
+    -- perhaps sufficient if we know whether we are in a script tag
+    _unsanitaryTagStack  :: [Tag Text] -- stack of open unsanitary tags
   , _lastOpenTagPosition :: (Int, Int) -- row, col
-  , _lastOpenTag :: Tag Text
+  , _lastOpenTag         :: Tag Text
   } deriving Show
 
 makeLenses ''XssState
 
 data XssConfig = XssConfig {
     _parseOptions :: ParseOptions Text
-  } 
+  }
 
 type XssRWS = RWS XssConfig [XssFlag] XssState
 
@@ -47,4 +49,4 @@ reportUnsafe msg = do
     let pos = st ^. lastOpenTagPosition
     let tag = st ^. lastOpenTag
     tell [XssFlag pos tag msg]
- 
+
